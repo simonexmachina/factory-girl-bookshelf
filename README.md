8f3ff65e710a18adc7f7b8b6142fbf31c1994045
Simon Wade <simon.wade@gmail.com>
2014-05-15 21:40:28 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# factory-girl-bookshelf
+
+A [Bookshelf](http://bookshelfjs.org/) adapter for [factory-girl](https://github.com/aexmachina/factory-girl).
+
+## Usage
+
+```javascript
+require('factory-girl-bookshelf')();
+```
+
+Or, if you want to specify which models it should be used for:
+
+```javascript
+require('factory-girl-bookshelf')(['User', 'Foo', 'Bar']);
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+var Factory = require('factory-girl'),
+    Adapter = Factory.Adapter;
+
+var BookshelfAdapter = function() {};
+BookshelfAdapter.prototype = new Adapter();
+BookshelfAdapter.prototype.build = function(Model, props) {
+  return new Model(props);
+};
+BookshelfAdapter.prototype.save = function(doc, Model, cb) {
+  doc.save().nodeify(cb);
+};
+
+var adapter = new BookshelfAdapter();
+
+module.exports = function(models) {
+  if (models) {
+    for (var i = 0; i < models.length; i++) {
+      Factory.setAdapter(adapter, models[i]);
+    }
+  }
+  else {
+    Factory.setAdapter(adapter);
+  }
+};
