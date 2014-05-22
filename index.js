3f41d15480901e1cc4bfdf261e65520bd62d9486
Simon Wade <simon.wade@gmail.com>
2014-05-22 11:38:02 +1000

    Don't attempt to destroy if already destroyed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Factory = require('factory-girl'),
-    Adapter = Factory.Adapter;
+var factory = require('factory-girl'),
+    Adapter = factory.Adapter;
 
 var BookshelfAdapter = function() {};
 BookshelfAdapter.prototype = new Adapter();
@@ -10,18 +10,18 @@ BookshelfAdapter.prototype.save = function(doc, Model, cb) {
   doc.save().nodeify(cb);
 };
 BookshelfAdapter.prototype.destroy = function(doc, Model, cb) {
+  if (!doc.id) return process.nextTick(cb);
   doc.destroy().nodeify(cb);
 };
 
 var adapter = new BookshelfAdapter();
-
 module.exports = function(models) {
   if (models) {
     for (var i = 0; i < models.length; i++) {
-      Factory.setAdapter(adapter, models[i]);
+      factory.setAdapter(adapter, models[i]);
     }
   }
   else {
-    Factory.setAdapter(adapter);
+    factory.setAdapter(adapter);
   }
 };
