8b0b1e88595b308ec9cf66b46f60b6db36736dcf
Simon Wade <simon.wade@gmail.com>
2015-08-17 21:17:39 +1000

    Update versions and added tests and docs for relationships

--- README.md
+++ README.md
@@ -6,12 +6,32 @@ A [Bookshelf](http://bookshelfjs.org/) adapter for [factory-girl](https://github
 
 ## Usage
 
-```javascript
+```js
 require('factory-girl-bookshelf')();
 ```
 
 Or, if you want to specify which models it should be used for:
 
-```javascript
+```js
 require('factory-girl-bookshelf')(['User', 'Foo', 'Bar']);
+```
+
+Relationships can be created using `assoc`:
+
+```js
+var Model = Bookshelf.db.Model;
+var Parent = Model.extend({
+  children: function() {
+    return this.hasMany(Child, 'parent_id');
+  }
+});
+var Child = Model.extend({
+  parent: function() {
+    return this.belongsTo(Parent, 'parent_id');
+  }
+});
+factory.define('parent', Parent);
+factory.define('child', Child, {
+  parent_id: factory.assoc('parent', 'id')
+});
 ```
--- test/adapter.js
+++ test/adapter.js
@@ -1,21 +1,62 @@
 var adapter = require('..')();
-var Bookshelf = require('bookshelf');
+var Factory = require('factory-girl').Factory;
 var tests = require('factory-girl/lib/adapter-tests');
 var config = require('config-node')();
 var Bookshelf = require('bookshelf');
+var Model = Bookshelf.db.Model;
 
 describe('Bookshelf adapter', function() {
   init();
 
-  var Model = Bookshelf.db.Model.extend({
+  var TestModel = Model.extend({
     tableName: 'test'
   });
-  tests(adapter, Model, countModels);
+
+  tests(adapter, TestModel, countModels);
   function countModels(cb) {
-    Model.query().count('id as count').then(function(result) {
+    TestModel.query().count('id as count').then(function(result) {
       return cb(null, result[0].count);
     }, cb);
   }
+
+  describe('assoc just works', function() {
+    var factory, Parent, Child;
+    before(function() {
+      factory = new Factory();
+      factory.setAdapter(adapter);
+
+      Parent = Model.extend({
+        tableName: 'test',
+        children: function() {
+          return this.hasMany(Child, 'parent_id');
+        }
+      });
+      Child = Model.extend({
+        tableName: 'children',
+        parent: function() {
+          return this.belongsTo(Parent, 'parent_id');
+        }
+      });
+
+      factory.define('parent', Parent);
+      factory.define('child', Child, {
+        parent_id: factory.assoc('parent', 'id')
+      });
+    });
+    it('creates hasMany', function(done) {
+      factory.create('child', function(err, child) {
+          if (err) return done(err);
+          child.should.be.an.instanceof(Child);
+          child.parent().fetch({withRelated: 'children'})
+            .then(function(parent) {
+              parent.should.be.an.instanceof(Parent);
+              parent.related('children').length.should.equal(1);
+              done();
+            });
+      });
+    });
+  });
+
 });
 
 function init() {
@@ -25,9 +66,18 @@ function init() {
   before(function() {
     return knex.schema.dropTableIfExists('test')
       .then(function() {
+        return knex.schema.dropTableIfExists('test')
+      })
+      .then(function() {
         return knex.schema.createTable('test', function(table) {
           table.increments();
           table.string('name');
+        });
+      })
+      .then(function () {
+        return knex.schema.createTable('children', function(table) {
+          table.increments();
+          table.integer('parent_id').references('test.id');
         });
       });
   });
